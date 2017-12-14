d5c3bceea25898c1bb39c1c332031413a2902d59
Dylan Moylan <dylanmoylan1@gmail.com>
2017-12-14 14:01:45 -0500

    Builds out initial migration and continues backend file structure prep.

---
+++ db/migrations/migration-133912142017.sql
@@ -0,0 +1,35 @@
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS groups;
+DROP TABLE IF EXISTS standups;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  password_digest TEXT NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  permissions INT,
+  avatar VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS standups (
+  id SERIAL PRIMARY KEY,
+  graph_position INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS groups (
+  id SERIAL PRIMARY KEY,
+  group_name VARCHAR(255),
+  owner_id INTEGER REFERENCES users(id) NOT NULL,
+  user_id INTEGER REFERENCES users(id) NOT NULL,
+  parent_group INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS comments (
+  id SERIAL PRIMARY KEY,
+  comment_body TEXT,
+  from_id REFERENCES users(id) NOT NULL,
+  to_id REFERENCES standups(id) NOT NULL,
+  time_created TIMESTAMP,
+  comment_type VARCHAR(255)
+);
