f49b4f27da1e0c21bf13139d48c6840df95173c8
Dylan Moylan <dylanmoylan1@gmail.com>
2017-12-19 01:40:23 -0500

    Removes fontawesome dependency because the module sucks.

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import FontAwesome from 'react-fontawesome'
 
 function Footer(props) {
   return (
     <footer>
       <div>
         <ul>
-          <li><a href="https://github.com/dylonion/Standups"><FontAwesome name="github" /> Github</a></li>
+          <li><a href="https://github.com/dylonion/Standups">Github</a></li>
           <li></li>
         </ul>
       </div>
